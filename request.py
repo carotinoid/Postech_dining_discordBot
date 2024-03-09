1325a35545cfc23a5c684e508a9e433481776b60
Powering111 <50047351+Powering111@users.noreply.github.com>
2024-03-09 21:26:36 +0900

    Add KAIST dining feature

--- request.py
+++ request.py
@@ -3,13 +3,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-# get response
-res = requests.get("https://dining.postech.ac.kr/")
-soup = BeautifulSoup(res.text, 'html.parser')
-
-cards = soup.select('.card-content') # 0 조식 1 중식 2 석식
-
 def get_meal(idx):
+    idx_postech = [0,1,2]
+    idx_kaist = [3,4,5]
+    if idx in idx_postech:
+        return get_meal_postech(idx)
+    elif idx in idx_kaist:
+        return get_meal_kaist(idx)
+
+def get_meal_postech(idx):
+    # get response
+    res = requests.get("https://dining.postech.ac.kr/")
+    soup = BeautifulSoup(res.text, 'html.parser')
+
+    cards = soup.select('.card-content') # 0 조식 1 중식 2 석식
+
     regular_meal = cards[idx].select("div.card-content div.content")[0]
     meal_list = str(regular_meal).strip("<div class=\"content\">").strip("</div>").split("<br/><br/>")
     if idx == 0:
@@ -38,12 +46,37 @@ def get_meal(idx):
 
     return meal_str_list
 
+def get_meal_kaist(idx):
+    # get response
+    res = requests.get("https://www.kaist.ac.kr/kr/html/campus/053001.html?dvs_cd=fclt")
+    soup = BeautifulSoup(res.text, 'html.parser')
+    
+    cards = soup.select("#tab_item_1 td")
+    
+    def conv(x):
+        x = x.replace("<td>","").replace("<ul class=\"list-1st\">","").strip().replace("\r","").replace("\t","").replace("\n","").replace("&amp;","&")
+        b_index = x.rfind("(")
+        if b_index!=-1:
+            x = x[:b_index]
+        c_index = x.find("-->")
+        if c_index!=-1:
+            x = x[c_index+3:]
+        return x
+    def filt(x):
+        print("filtering",x)
+        return not ("<" in x or len(x)==0 or x=="kcal" or x.isdigit())
+    a = list(filter(filt, map(conv,str(cards[idx-3]).strip().split("<br/>"))))
+    print(a)
+    return a
+
+
+
 def save_cache():
-    for i in [0,1,2]:
+    for i in [0,1,2,3,4,5]:
         with open("meal_cache_"+str(i), "w", encoding='utf8') as f:
             f.write("\n")
             f.write("------------------------------------\n")
-            f.write("**["+["아침","점심","저녁"][i]+"]**\n")
+            f.write("**["+["아침","점심","저녁"][i%3]+"]**\n")
             for meal in get_meal(i):
                 f.write("- " + meal + "\n")
             f.write("------------------------------------")
--- send.py
+++ send.py
@@ -4,10 +4,27 @@ import request
 import requests
 import datetime
 
-def send_discord(idx):
-    cache_path = "meal_cache_"+str(idx)
+# feature control
+webhook_postech = "webhook"
+feature_postech = os.path.isfile(webhook_postech)
+idx_postech = [0,1,2]
 
+webhook_kaist = "webhook2"
+feature_kaist = os.path.isfile(webhook_kaist)
+idx_kaist = [3,4,5]
+
+def send_discord(idx):
+    # determine the request type
+    if idx in idx_postech and feature_postech:
+        webhook_path = webhook_postech
+    elif idx in idx_kaist and feature_kaist:
+        webhook_path = webhook_kaist
+    else:
+        print("Error: feature is not enabled.")
+        return
+    
     # check if cache exists
+    cache_path = "meal_cache_"+str(idx)
     
     last_cache_datetime = datetime.datetime.strptime(time.ctime(os.path.getmtime(cache_path)),"%a %b %d %H:%M:%S %Y")
     today_datetime = datetime.datetime.today()
@@ -18,7 +35,7 @@ def send_discord(idx):
         print("saving cache")
         request.save_cache()
     
-    with open("webhook", "r") as f:
+    with open(webhook_path, "r") as f:
         _webhook = f.readline()
     with open(cache_path, "r", encoding='utf8') as f:
         content = "> ".join(f.readlines())
@@ -38,4 +55,7 @@ if __name__=="__main__":
     else:
         idx = 2
 
+    # POSTECH
     send_discord(idx)
+    # KAIST
+    send_discord(idx+3)
